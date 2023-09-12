4246c5809b4fad3c350c221c69dd901a7f45629b
ivanvinicius <ivanvinicius@github.com>
2023-09-12 20:03:10 -0300

    :sparkles: structure

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { fastify } from 'fastify'
+
+import { getAllPromptsRoute } from './routes/get-all-prompts'
+
+const app = fastify()
+
+app.register(getAllPromptsRoute)
+
+app.listen({ port: 3333 })
